2a28fe73d04fd3de6c200d952ea400f66a3535c0
MilanSavani010 <156356140+MilanSavani010@users.noreply.github.com>
2025-05-05 22:31:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 
 app.use((req, res, next) => {
     console.log('Client IP:', req.ip);
